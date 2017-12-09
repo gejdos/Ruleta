c89537ecc52c23964cc836d88df2349ea1a028ee
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-09 06:18:56 +0100

    commit

---
+++ Ruleta/Ruleta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruleta
+{
+    class Ruleta
+    {
+        int bank = 1000;
+        Random r = new Random();
+
+        private bool StavkaNaFarbu(string farba)
+        {
+            int cisloFarby = (farba == "cierna") ? 1 : 0;
+
+            return (r.Next(2) == cisloFarby);
+        }
+
+        private bool StavkaNaCislo(string cislo)
+        {
+            return (r.Next(37) == int.Parse(cislo));
+        }
+
+        private bool Parne(string cislo)
+        {
+            return ()
+        }
+
+
+
+    }
+}
